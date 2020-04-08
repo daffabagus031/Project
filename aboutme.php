52d00b3ed3c453bdcf6c0b5e49daad9150862033
Muhammad Daffa Bagus Muttaqin <63336347+daffabagus031@users.noreply.github.com>
2020-04-08 19:16:58 +0700

    Delete aboutme.php

--- aboutme.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Nama : Muhammad Daffa Bagus Muttaqin";
-echo "<br>";
-echo "Kelas : XI RPL 2";
-echo "<br>";
-echo "Sekolah : SMK Taruna Bhakti Depok";
-echo "<br>";
-echo "alamat : Jl. Kawi III No. 159 Kel Abadijaya Kec Sukmajaya Depok";
-?>
