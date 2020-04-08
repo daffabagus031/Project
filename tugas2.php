121f6a94033f06401e8d0833332b1431173527d6
Muhammad Daffa Bagus Muttaqin <63336347+daffabagus031@users.noreply.github.com>
2020-04-08 19:11:28 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Nama : Muhammad Daffa Bagus Muttaqin";
+    echo "<br>";
+    echo "Kelas : XI RPL 2";
+    echo "<br>";
+    echo "Sekolah : SMK Taruna Bhakti Depok";
+    echo "<br>";
+    echo "Alamat Rumah : Jl. Kawi III No. 159 Kel Abdijaya Kec Sukamajaya Depok";
+    ?>
+</body>
+</html>
