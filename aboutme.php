666ae2510660ebc85e07833a884f1968ecdf6c3c
Muhammad Daffa Bagus Muttaqin <63336347+daffabagus031@users.noreply.github.com>
2020-04-09 09:09:35 +0700

    Update aboutme.php

--- aboutme.php
+++ aboutme.php
@@ -1,11 +1,7 @@
 <?php
 echo "Nama : Muhammad Daffa Bagus Muttaqin";
-echo "<br>";
 echo "Kelas : XI RPL 2";
-echo "<br>";
 echo "Sekolah : SMK Taruna Bhakti Depok";
-echo "<br>";
 echo "No Tlp : 087877261228";
-echo "<br>";
 echo "Email : daffabagus031@gmail.com";
 ?>    
