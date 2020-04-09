e75c7c5f3cec875e3dafd381e79036d244009f24
Muhammad Daffa Bagus Muttaqin <63336347+daffabagus031@users.noreply.github.com>
2020-04-09 09:08:41 +0700

    Update tugas2.php

--- tugas2.php
+++ tugas2.php
@@ -1,9 +1,6 @@
 <?php
 echo "Nama : Muhammad Daffa Bagus Muttaqin";
-echo "<br>";
 echo "Kelas : XI RPL 2";
-echo "<br>";
 echo "Sekolah : SMK Taruna Bhakti Depok";
-echo "<br>";
 echo "Alamat Rumah : Jl. Kawi III No. 159 Kel Abdijaya Kec Sukamajaya Depok";
 ?>
